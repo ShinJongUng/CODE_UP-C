416c2f7b1c21ec5e9841947109328c997413f302
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-09-09 15:32:26 +0900

    Add files via upload

---
+++ 1069_21324277(AC).c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main(void) {
+  char a;
+  scanf("%s", &a);
+  
+  switch(a){
+    case 'A':
+    printf("best!!!");
+    break;
+    case 'B':
+    printf("good!!");
+    break;
+    case 'C':
+    printf("run!");
+    break;
+    case 'D':
+    printf("slowly~");
+    break;
+    default:
+    printf("what?");
+    break;
+  }
+  
+}
