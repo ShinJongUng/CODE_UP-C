0a194cc87cbee89e9973b1d4fbb1d5b6b3f2a774
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-09-11 00:31:06 +0900

    Add files via upload

---
+++ 1080_21358136(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+  int n, value = 0;
+  scanf("%d", &n);
+  int i = 0;
+ while(n >= value){
+   i++;
+   value += i;
+   if(n == value){
+     i--;
+   }
+ }
+ printf("%d", i);
+}
