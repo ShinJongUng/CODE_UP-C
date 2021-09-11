58f9a7e47803bc35213baf48b7d82392864f07f7
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-09-11 18:49:08 +0900

    Add files via upload

---
+++ 1090_21368109(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+  long long int n, m, k, cnt=0;
+  scanf("%lld %lld %lld", &n, &m, &k);
+  long long int i;
+  for(i =n;; i*=m){
+    cnt++;
+    if(k == cnt){
+      break;
+    }
+  }
+  printf("%lld", i);
+}
+
