18a809e33659ed60e58447aad1fb4ac94cf8cbfb
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-09-11 18:30:02 +0900

    Add files via upload

---
+++ 1089_21367796(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+  int n, m, k, cnt=0;
+  scanf("%d %d %d", &n, &m, &k);
+  int i;
+  for(i =n;; i+=m){
+    cnt++;
+    if(k == cnt){
+      break;
+    }
+  }
+  printf("%d", i);
+}
+
