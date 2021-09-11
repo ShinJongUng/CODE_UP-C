03be249a82f3950bcf87f19efad9cf73fa37d5ca
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-09-11 17:15:19 +0900

    Add files via upload

---
+++ 1083_21366810(AC).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+  int n;
+  scanf("%d", &n); 
+  for(int i=1; i<=n; i++){
+    if(i%3 == 0){
+      printf("X ");
+    }
+    else{
+      printf("%d ", i);
+    }
+  }
+}
+
