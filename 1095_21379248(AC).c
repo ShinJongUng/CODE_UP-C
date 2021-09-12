07a72f235f19a5a92a8aa13687ea02cc5ffb7154
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-09-12 20:34:42 +0900

    Add files via upload

---
+++ 1095_21379248(AC).c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void) {
+  int n, i , min=23;
+  int a[10000]={};
+  scanf("%d", &n); 
+  for(i=1; i<=n; i++) 
+  {
+    scanf("%d", &a[i]);
+  }
+  for(i=1; i<=n; i++)
+  {
+    if(min >= a[i])
+      min = a[i];
+  }
+  printf("%d", min);
+}
