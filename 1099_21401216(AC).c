bb43743d819b2ea91679d4618efd79841fc25438
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2021-09-13 17:40:18 +0900

    Add files via upload

---
+++ 1099_21401216(AC).c
@@ -0,0 +1,32 @@
+﻿#include<stdio.h> 
+int main(){ 
+    int board[10][10]={0,}; 
+    int x=1, y=1; int i, j;
+    for (i = 0; i < 10; i++) { 
+        for (j = 0; j < 10; j++)
+        { 
+            scanf("%d", &board[i][j]); 
+            
+        } 
+        
+    } 
+    while (board[y][x] != 2 && !(x == 8 && y == 8)) {
+        board[y][x] = 9; if (board[y][x+1] == 1) 
+        { 
+            y++; } 
+            else { 
+                x++; 
+            } 
+        
+    } 
+    board[y][x] = 9; 
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("%d ", board[i][j]); 
+            
+        } 
+        printf("\n"); 
+        
+    } 
+    return 0;
+}
